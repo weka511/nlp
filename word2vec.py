c7fbaa662d23e350fb59ead112103a2e06d1fe67
Simon Crase <simon@greenweaves.nz>
2021-06-08 15:03:46 +1200

    Test by listing similar words

--- word2vec.py
+++ word2vec.py
@@ -15,24 +15,29 @@
 
 # https://towardsdatascience.com/implementing-word2vec-in-pytorch-skip-gram-model-e6bae040d2fb
 
+from argparse            import ArgumentParser
 from itertools           import chain
-from matplotlib.pyplot   import figure, legend, plot, savefig, show, xlabel, ylabel
+from matplotlib.pyplot   import figure, legend, plot, savefig, show, title, xlabel, ylabel
 from numpy               import array
 from sys                 import float_info
-from torch               import dot, from_numpy, load, matmul, norm, randn, save, zeros
+from torch               import dot, flip, from_numpy, load, matmul, norm, randn,  save, zeros
 from torch.autograd      import Variable
 from torch.nn.functional import log_softmax, nll_loss
 
-
+# tokenize_corpus
 
 def tokenize_corpus(corpus):
     return [x.split() for x in corpus]
+
+# create_vocabulary
 
 def create_vocabulary(tokenized_corpus):
     vocabulary = {token for sentence in tokenized_corpus for token in sentence}
     return vocabulary,                                     \
            {w: idx for (idx, w) in enumerate(vocabulary)}, \
            {idx: w for (idx, w) in enumerate(vocabulary)}
+
+# create_idx_pairs
 
 def create_idx_pairs(tokenized_corpus, word2idx,
                      window_size = 2):
@@ -51,10 +56,14 @@ def create_idx_pairs(tokenized_corpus, word2idx,
 
     return array(idx_pairs)
 
+# get_input_layer
+
 def get_input_layer(word_idx,vocabulary_size):
     x           = zeros(vocabulary_size).float()
     x[word_idx] = 1.0
     return x
+
+# train
 
 def train(idx_pairs,vocabulary_size,
           lr             = 0.01,
@@ -93,40 +102,46 @@ def train(idx_pairs,vocabulary_size,
 
     return W1,W2,Epochs,Losses
 
+def get_similarity(v,u):
+    return dot(v,u)/(norm(v)*norm(u))
+
+# compare
+
 # https://gist.github.com/mbednarski/da08eb297304f7a66a3840e857e060a0#gistcomment-3689982
 
 def compare(word1,word2,
             W1              = None,
             vocabulary_size = 0,
             word2idx        = {}):
-    def get_similarity(v,u):
-        return dot(v,u)/(norm(v)*norm(u))
+
 
     s = get_similarity(matmul(W1,get_input_layer(word2idx[word1],vocabulary_size)),
                        matmul(W1,get_input_layer(word2idx[word2],vocabulary_size)))
     print (f'{word1} {word2} {s}')
     return s
 
+# corpus
+
 def corpus(file_name):
     with open(file_name) as f:
         for line in f:
             yield line.strip('.\n')
 
+
+
 if __name__=='__main__':
-    from argparse import ArgumentParser
     parser = ArgumentParser('Build word2vector')
-    parser.add_argument('action',      choices=['train', 'test'])
-    parser.add_argument('--N',         type = int,   default = 20001,              help = 'Number of Epochs for training')
-    parser.add_argument('--lr',        type = float, default = 0.01,               help = 'Learning rate (before decay)')
-    parser.add_argument('--decay',     type = float, default = [0.005], nargs='+', help = 'Decay rate for learning')
-    parser.add_argument('--frequency', type = int,   default = 100,                help = 'Frequency for display')
-    parser.add_argument('--n',         type = int,   default = 2,                  help = 'Window size')
-    parser.add_argument('--m',         type = int,   default = 5,                  help ='Embedding size')
-    parser.add_argument('--output',                  default = 'out',              help = 'Output file name')
-    parser.add_argument('--burn',      type=int,     default = None,               help = 'Burn in')
-    parser.add_argument('--show',                    default = False, action='store_true')
-    parser.add_argument('--pairs',                   default = False, action='store_true')
-    parser.add_argument('--corpus',                  default = 'nano-corpus.txt',  help = 'Corpus file name')
+    parser.add_argument('action',      choices=['train', 'test'],                          help = 'Train weights or test them')
+    parser.add_argument('--N',         type = int,   default = 20001,                      help = 'Number of Epochs for training')
+    parser.add_argument('--lr',        type = float, default = 0.01,                       help = 'Learning rate (before decay)')
+    parser.add_argument('--decay',     type = float, default = [0.005], nargs='+',         help = 'Decay rate for learning')
+    parser.add_argument('--frequency', type = int,   default = 100,                        help = 'Frequency for display')
+    parser.add_argument('--n',         type = int,   default = 2,                          help = 'Window size')
+    parser.add_argument('--m',         type = int,   default = 5,                          help ='Embedding size')
+    parser.add_argument('--output',                  default = 'out',                      help = 'Output file name')
+    parser.add_argument('--burn',      type=int,     default = None,                       help = 'Burn in')
+    parser.add_argument('--show',                    default = False, action='store_true', help='Show plots')
+    parser.add_argument('--corpus',                  default = 'nano-corpus.txt',          help = 'Corpus file name')
     args = parser.parse_args()
 
     if args.action == 'train':
@@ -135,9 +150,6 @@ if __name__=='__main__':
         vocabulary_size              = len(vocabulary)
         idx_pairs                    = create_idx_pairs(tokenized_corpus, word2idx,
                                                         window_size = args.n)
-        if args.pairs:
-            for a,b in idx_pairs:
-                print (f'<{idx2word[a]},{idx2word[b]}>')
 
         figure(figsize=(10,10))
 
@@ -167,6 +179,7 @@ if __name__=='__main__':
         xlabel('Epoch')
         ylabel('Loss')
         legend()
+        title(f'{args.corpus} -- Embedding dimenions={args.m}')
         savefig(args.output)
 
     if args.action == 'test':
@@ -178,12 +191,13 @@ if __name__=='__main__':
         idx_pairs         = loaded['idx_pairs']
 
         _,vocabulary_size = W1.shape
-        for a,b in idx_pairs:
-            compare(idx2word[a], idx2word[b],
-                    W1              = W1,
-                    vocabulary_size = vocabulary_size,
-                    word2idx        = word2idx)
 
+        for idx, word in idx2word.items():
+            word_vector      = W1[:,idx]
+            sims             = matmul(word_vector,W1)
+            most_similar_ids = flip(sims.argsort(),[0])
+            sim_words        = [idx2word[i] for i in most_similar_ids.tolist()]
+            print (f'{word}\t{" ".join([s for s in sim_words])}')
 
     if args.show:
         show()
