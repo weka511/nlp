aa26dc1a68b2ba4efaf0f490d05dddca3de733e3
Simon Crase <simon@greenweaves.nz>
2021-06-08 14:17:10 +1200

    Moved nano-corpus to text file

--- word2vec.py
+++ word2vec.py
@@ -1,8 +1,24 @@
+#    Copyright (C) 2021 Simon A. Crase
+#
+#    This is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This software is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 # https://towardsdatascience.com/implementing-word2vec-in-pytorch-skip-gram-model-e6bae040d2fb
 
 from itertools           import chain
 from matplotlib.pyplot   import figure, legend, plot, savefig, show, xlabel, ylabel
 from numpy               import array
+from sys                 import float_info
 from torch               import dot, from_numpy, load, matmul, norm, randn, save, zeros
 from torch.autograd      import Variable
 from torch.nn.functional import log_softmax, nll_loss
@@ -14,7 +30,9 @@ def tokenize_corpus(corpus):
 
 def create_vocabulary(tokenized_corpus):
     vocabulary = {token for sentence in tokenized_corpus for token in sentence}
-    return vocabulary, {w: idx for (idx, w) in enumerate(vocabulary)}, {idx: w for (idx, w) in enumerate(vocabulary)}
+    return vocabulary,                                     \
+           {w: idx for (idx, w) in enumerate(vocabulary)}, \
+           {idx: w for (idx, w) in enumerate(vocabulary)}
 
 def create_idx_pairs(tokenized_corpus, word2idx,
                      window_size = 2):
@@ -25,8 +43,8 @@ def create_idx_pairs(tokenized_corpus, word2idx,
         indices = [word2idx[word] for word in sentence]
 
         for center_word_pos in range(len(indices)):
-            for w in chain(range(-window_size,0), range(1, window_size + 1)):
-                context_word_pos = center_word_pos + w
+            for word_offset in chain(range(-window_size,0), range(1, window_size + 1)):
+                context_word_pos = center_word_pos + word_offset
                 if context_word_pos >= 0 and context_word_pos < len(indices):
                     context_word_idx = indices[context_word_pos]
                     idx_pairs.append((indices[center_word_pos], context_word_idx))
@@ -34,7 +52,7 @@ def create_idx_pairs(tokenized_corpus, word2idx,
     return array(idx_pairs)
 
 def get_input_layer(word_idx,vocabulary_size):
-    x = zeros(vocabulary_size).float()
+    x           = zeros(vocabulary_size).float()
     x[word_idx] = 1.0
     return x
 
@@ -76,14 +94,23 @@ def train(idx_pairs,vocabulary_size,
     return W1,W2,Epochs,Losses
 
 # https://gist.github.com/mbednarski/da08eb297304f7a66a3840e857e060a0#gistcomment-3689982
-def get_similarity(v,u):
-    return dot(v,u)/(norm(v)*norm(u))
 
-def compare(word1,word2,W1,vocabulary_size):
-    w1v = matmul(W1,get_input_layer(word2idx[word1],vocabulary_size))
-    w2v = matmul(W1,get_input_layer(word2idx[word2],vocabulary_size))
-    s   = get_similarity(w1v,w2v)
+def compare(word1,word2,
+            W1              = None,
+            vocabulary_size = 0,
+            word2idx        = {}):
+    def get_similarity(v,u):
+        return dot(v,u)/(norm(v)*norm(u))
+
+    s = get_similarity(matmul(W1,get_input_layer(word2idx[word1],vocabulary_size)),
+                       matmul(W1,get_input_layer(word2idx[word2],vocabulary_size)))
     print (f'{word1} {word2} {s}')
+    return s
+
+def corpus(file_name):
+    with open(file_name) as f:
+        for line in f:
+            yield line.strip('.\n')
 
 if __name__=='__main__':
     from argparse import ArgumentParser
@@ -97,27 +124,24 @@ if __name__=='__main__':
     parser.add_argument('--m',         type = int,   default = 5,                  help ='Embedding size')
     parser.add_argument('--output',                  default = 'out',              help = 'Output file name')
     parser.add_argument('--burn',      type=int,     default = None,               help = 'Burn in')
-    parser.add_argument('--show',                    default = False,              action='store_true')
+    parser.add_argument('--show',                    default = False, action='store_true')
+    parser.add_argument('--pairs',                   default = False, action='store_true')
+    parser.add_argument('--corpus',                  default = 'nano-corpus.txt',  help = 'Corpus file name')
     args = parser.parse_args()
 
-    corpus = [
-        'he is a king',
-        'she is a queen',
-        'he is a man',
-        'she is a woman',
-        'warsaw is poland capital',
-        'berlin is germany capital',
-        'paris is france capital',
-    ]
-
     if args.action == 'train':
-        tokenized_corpus             = tokenize_corpus(corpus)
+        tokenized_corpus             = tokenize_corpus(corpus(args.corpus))
         vocabulary,word2idx,idx2word = create_vocabulary(tokenized_corpus)
         vocabulary_size              = len(vocabulary)
         idx_pairs                    = create_idx_pairs(tokenized_corpus, word2idx,
                                                         window_size = args.n)
+        if args.pairs:
+            for a,b in idx_pairs:
+                print (f'<{idx2word[a]},{idx2word[b]}>')
 
         figure(figsize=(10,10))
+
+        minimum_loss = float_info.max
 
         for decay_rate in args.decay:
             W1,W2,Epochs,Losses = train(idx_pairs,vocabulary_size,
@@ -128,26 +152,38 @@ if __name__=='__main__':
                                         embedding_dims = args.m,
                                         frequency      = args.frequency)
             plot(Epochs,Losses,label=f'Decay rate={decay_rate}')
-            save(W1,f'{args.output}-W1.pt')
-            save(W2,f'{args.output}-W2.pt')
-            save(word2idx,f'{args.output}-word2idx.pt')
+
+            if Losses[-1]<minimum_loss:
+                minimum_loss = Losses[-1]
+                print (f'Saving weights for Loss={minimum_loss} in {args.output}.pt')
+                save ({'W1'         : W1,
+                       'W2'         : W2,
+                       'word2idx'   : word2idx,
+                       'idx2word'   : idx2word,
+                       'decay_rate' : decay_rate,
+                       'idx_pairs'  : idx_pairs},
+                      f'{args.output}.pt')
+
         xlabel('Epoch')
         ylabel('Loss')
         legend()
         savefig(args.output)
 
     if args.action == 'test':
-        W1                = load(f'{args.output}-W1.pt')
-        W2                = load(f'{args.output}-W2.pt')
-        word2idx          = load(f'{args.output}-word2idx.pt')
+        loaded            = load(f'{args.output}.pt')
+        W1                = loaded['W1']
+        W2                = loaded['W2']
+        word2idx          = loaded['word2idx']
+        idx2word          = loaded['idx2word']
+        idx_pairs         = loaded['idx_pairs']
+
         _,vocabulary_size = W1.shape
-        compare('he',      'king',  W1, vocabulary_size)
-        compare('she',     'queen', W1, vocabulary_size)
-        compare('she',    'king',   W1, vocabulary_size)
-        compare('he',     'queen',  W1, vocabulary_size)
-        compare('warsaw', 'king',   W1, vocabulary_size)
-        compare('warsaw', 'poland', W1, vocabulary_size)
-        compare('poland', 'capital', W1, vocabulary_size)
-        compare('france', 'capital', W1, vocabulary_size)
+        for a,b in idx_pairs:
+            compare(idx2word[a], idx2word[b],
+                    W1              = W1,
+                    vocabulary_size = vocabulary_size,
+                    word2idx        = word2idx)
+
+
     if args.show:
         show()
