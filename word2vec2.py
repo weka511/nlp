455bb3a5231ae78f66d310c227fa005f6d7e6419
Simon Crase <simon@greenweaves.nz>
2023-06-10 14:50:31 +1200

    #13 Creating examples

--- word2vec2.py
+++ word2vec2.py
@@ -23,27 +23,34 @@ from csv import writer
 from glob import glob
 from time import time
 import numpy as np
-from skipgram import Vocabulary, ExampleBuilder
+from skipgram import Vocabulary, ExampleBuilder, Tower
 from tokenizer import read_text, extract_sentences, extract_tokens
 
 if __name__=='__main__':
     start  = time()
-    parser = ArgumentParser(__doc__)
+    parser = ArgumentParser(description=__doc__)
     parser.add_argument('docnames', nargs='+', help='A list of documents to be processed')
-    parser.add_argument('--action')
+    parser.add_argument('--action', choices=['create', 'train'], required=True)
     parser.add_argument('--examples', default='examples.csv')
     args = parser.parse_args()
-    docnames = [doc for pattern in args.docnames for doc in glob(pattern)]
-    vocabulary = Vocabulary()
-    word2vec = ExampleBuilder()
-    with open(args.examples,'w') as out:
-        examples = writer(out)
-        for sentence in extract_sentences(extract_tokens(read_text(file_names = docnames))):
-            indices = vocabulary.parse(sentence)
-            for word,context in word2vec.generate_positive_examples([indices]):
-                examples.writerow([word,context,+1])
-            for word,context  in word2vec.create_negative_examples(ExampleBuilder.normalize(vocabulary)):
-                examples.writerow([word,context,-1])
+
+    match args.action:
+        case 'create':
+            docnames = [doc for pattern in args.docnames for doc in glob(pattern)]
+            vocabulary = Vocabulary()
+            word2vec = ExampleBuilder()
+
+            with open(args.examples,'w', newline='') as out:
+                examples = writer(out)
+                for sentence in extract_sentences(extract_tokens(read_text(file_names = docnames))):
+                    indices = vocabulary.parse(sentence)
+                    for word,context in word2vec.generate_positive_examples([indices]):
+                        examples.writerow([word,context,+1])
+                for word,context in  word2vec.create_negative_examples(ExampleBuilder.normalize(vocabulary)):
+                    examples.writerow([word,context,-1])
+
+        case 'train':
+            print ('Not implemented')
 
 
     elapsed = time() - start
