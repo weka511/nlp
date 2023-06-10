96db535fc73f53c2d87a150fe524d71c0e7c2732
Simon Crase <simon@greenweaves.nz>
2023-06-10 17:27:38 +1200

    #13 fixed typo in template

--- template.py
+++ template.py
@@ -23,7 +23,7 @@ import numpy as np
 
 if __name__=='__main__':
     start  = time()
-    parser = ArgumentParser(__doc__)
+    parser = ArgumentParser(description=__doc__)
 
     args = parser.parse_args()
 
