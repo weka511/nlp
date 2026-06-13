c1085b296964e35d386a8cae5235afd251e4534e
Simon Crase <simon@greenweaves.nz>
2026-06-13 19:40:12 +1200

    #42 Generate ngrams

--- ngram.py
+++ ngram.py
@@ -22,14 +22,42 @@ from glob import glob
 from os.path import join
 from time import time
 import numpy as np
-from tokenizer import generate_sentences, generate_text, generate_tokens
+from tokenizer import generate_sentences, generate_text, generate_tokens,Token
 
 class Ngram:
     def __init__(self,n):
         self.n = n
+        self.vocabulary = {}
+        self.tuples = {}
+        self.symbols = []
+        
     def build(self,sentence):
-        pass
+        for t in self.generate_tuples([-1]*(self.n-1) +
+                                      [self.add_word(token) for token in sentence if self.is_word(token)] +
+                                      [-1]*(self.n-1)):
+            try:
+                self.tuples[t] += 1
+            except KeyError:
+                self.tuples[t] = 1
+        
+    def generate_tuples(self,tokens):
+        for i in range(len(tokens)-self.n + 1):
+            yield tuple(tokens[i:i+self.n])
+
+    def is_word(self,token):
+        return token.replace(Token.Apostrophe,'').isalpha()
     
+    def add_word(self,token):
+        try:
+            return self.vocabulary[token]
+        except KeyError:
+            self.symbols.append(token)
+            self.vocabulary[token] = len(self.vocabulary)
+            return self.vocabulary[token]
+
+    def get_ngram(self,key):
+        return [self.symbols[i] for i in key if i > -1]
+        
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--corpus', default=None, nargs='+', help='Name(s) of corpus file(s)')
@@ -44,6 +72,10 @@ def main():
     file_names = [globbed for name in args.corpus for globbed in glob(join(args.data, name))]
     for sentence in generate_sentences(generate_tokens(generate_text(file_names=file_names))):
         ngram.build(sentence)
+    for key,value in ngram.tuples.items():
+        if value > 1:
+            print (ngram.get_ngram(key),value)
+            
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
