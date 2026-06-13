8cd4cc82a005e6d64dae9d5f2c3c0177a435572a
Simon Crase <simon@greenweaves.nz>
2026-06-13 20:54:23 +1200

    #42 Load and save ngrams

--- ngram.py
+++ ngram.py
@@ -19,50 +19,93 @@
 
 from argparse import ArgumentParser
 from glob import glob
+from io import BytesIO
 from os.path import join
+from pathlib import Path
+from pickle import dump, HIGHEST_PROTOCOL, load
 from time import time
 import numpy as np
 from tokenizer import generate_sentences, generate_text, generate_tokens,Token
 
 class Ngram:
+    '''
+    This class represents a collection of n-grams
+    
+    Attributes:
+        n
+        vocabulary
+        symbols
+        tuples
+    '''
     def __init__(self,n):
         self.n = n
         self.vocabulary = {}
         self.tuples = {}
         self.symbols = []
         
-    def build(self,sentence):
-        for t in self.generate_tuples([-1]*(self.n-1) +
-                                      [self.add_word(token) for token in sentence if self.is_word(token)] +
-                                      [-1]*(self.n-1)):
+    def add_sentence(self,sentence):
+        '''
+        Grow our list of tuples using thse that cen be extracted from sentence
+        
+        Parameters:
+            sentence
+        '''
+        for n_gram in self.generate_tuples( [-1]*(self.n-1) +
+                                            [self.tokenize(word) for word in sentence if self.is_word(word)] +
+                                            [-1]*(self.n-1)):
             try:
-                self.tuples[t] += 1
+                self.tuples[n_gram] += 1
             except KeyError:
-                self.tuples[t] = 1
+                self.tuples[n_gram] = 1
         
     def generate_tuples(self,tokens):
+        '''
+        Extract typles of length n from a tokenized sentence
+        
+        Parameters:
+            tokens
+        '''
         for i in range(len(tokens)-self.n + 1):
             yield tuple(tokens[i:i+self.n])
 
     def is_word(self,token):
+        '''
+        Verify that a token is composed solely of letters and apostophes
+        
+        Parameters:
+            token
+        '''
         return token.replace(Token.Apostrophe,'').isalpha()
     
-    def add_word(self,token):
+    def tokenize(self,word):
+        '''
+        Convert a word to a token; if we haven't seeit it before, create a new token
+        
+        Parameters:
+            word
+        '''
         try:
-            return self.vocabulary[token]
+            return self.vocabulary[word]
         except KeyError:
-            self.symbols.append(token)
-            self.vocabulary[token] = len(self.vocabulary)
-            return self.vocabulary[token]
+            self.symbols.append(word)
+            self.vocabulary[word] = len(self.vocabulary)
+            return self.vocabulary[word]
 
-    def get_ngram(self,key):
-        return [self.symbols[i] for i in key if i > -1]
+    def get_ngram(self,tokens):
+        '''
+        Convert a tuple of tokens to display form
+        
+        Parameters:
+            tokens
+        '''
+        return tuple([self.symbols[i] for i in tokens if i > -1])
         
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--corpus', default=None, nargs='+', help='Name(s) of corpus file(s)')
     parser.add_argument('--data', default='./data')
     parser.add_argument('-n', '--n',default=3, type=int)
+    parser.add_argument('-', '--output',default=Path(__file__).stem)
     return parser.parse_args()
     
 def main():
@@ -71,11 +114,20 @@ def main():
     ngram = Ngram(args.n)
     file_names = [globbed for name in args.corpus for globbed in glob(join(args.data, name))]
     for sentence in generate_sentences(generate_tokens(generate_text(file_names=file_names))):
-        ngram.build(sentence)
-    for key,value in ngram.tuples.items():
-        if value > 1:
-            print (ngram.get_ngram(key),value)
-            
+        ngram.add_sentence(sentence)
+ 
+    output_file = (Path(args.data) / args.output).with_suffix('.pkl')
+    with open(output_file,'wb') as out:
+        dump(ngram, out, HIGHEST_PROTOCOL)
+        print (f'Saved ngrams to {output_file.resolve()}')
+
+    with open(output_file, 'rb') as inp:
+        ngram1 = load(inp) 
+        print (f'Loaded ngrams from {output_file.resolve()}')
+        for key,value in ngram1.tuples.items():
+            if value > 3:
+                print (ngram.get_ngram(key),value)
+                
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
