e293a041bcdb89effffa651074cc739237984544
Simon Crase <simon@greenweaves.nz>
2026-06-19 09:36:42 +1200

    #47 Added counter

--- vocabulary.py
+++ vocabulary.py
@@ -15,13 +15,16 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from unittest import main, TestCase, skip
+
 class Vocabulary:
     '''
-    Establish mapping between words and tokens
+    This class maps between words and tokens
     '''
     def __init__(self):
         self.symbols = []
         self.token = {}
+        self.counts = []
         
     def tokenize(self,word):
         '''
@@ -31,11 +34,14 @@ class Vocabulary:
             word     A string of characters comprising a single wird
         '''
         try:
-            return self.token[word]
+            token = self.token[word]
+            self.counts[token] += 1
         except KeyError:
             self.token[word] = len(self.symbols)
             self.symbols.append(word)
-            return self.token[word]    
+            token = self.token[word]
+            self.counts.append(1)
+        return token
         
     def get_word(self,token):
         '''
@@ -44,4 +50,33 @@ class Vocabulary:
         Parameters:
             token      An index into symbol table
         '''
-        return self.symbols[token]  
+        return self.symbols[token]
+    
+    def get_count(self,token):
+        '''
+        Determine the number of times a token appears in text
+
+        Parameters:
+            token      The word whose count we want
+        '''
+        return self.counts[token]
+
+ 
+    
+class TestVocabulary(TestCase):
+    def setUp(self):
+        self.vocabulary = Vocabulary()
+        for word in ['the', 'quick', 'brown','fox', 'jumps', 'over', 'the', 'lazy', 'dog',
+                          'that', 'guards', 'the', 'brown', 'cow']:
+            self.vocabulary.tokenize(word)
+            
+    def test_tokens(self):
+        self.assertEqual('cow',self.vocabulary.get_word(10))
+        
+    def test_count(self):                       
+        self.assertEqual(3,self.vocabulary.get_count(0))   # Number of 'the's
+        self.assertEqual(2,self.vocabulary.get_count(2))   # Number of 'brown's
+        self.assertEqual(1,self.vocabulary.get_count(3))   # Number of 'fox's    
+        
+if __name__ == '__main__':
+    main()
