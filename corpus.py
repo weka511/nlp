473493eb32a7c2e5409af26e3047994d85de491b
Simon Crase <simon@greenweaves.nz>
2026-08-05 08:52:41 +1200

    #88 Created thin wrapper around nltk/bnc

--- corpus.py
+++ corpus.py
@@ -15,7 +15,9 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-'''Read from BNC corpus'''
+'''
+    Read from BNC corpus
+'''
 
 from argparse import ArgumentParser
 from pathlib import Path
@@ -25,6 +27,26 @@ import numpy as np
 import nltk
 from nltk.corpus.reader.bnc import BNCCorpusReader
 
+class BNC:
+    '''
+        Read from BNC corpus
+    '''    
+    def __init__(self,root = r'C:\Users\weka5\nlp\data\2554\download\Texts'):
+        nltk.data.path.append(r'C:\Users\weka5\nlp\data\2554\download')
+        self.bnc = BNCCorpusReader(root=root,fileids= r'[A-K]/\w*/\w*\.xml')        
+    
+    def filenames(self):
+        for filename in self.bnc.fileids():
+            yield filename
+            
+    def words(self):
+        for word in self.bnc.words():
+            yield word
+            
+    def sentences(self,filename='A/A0/A00.xml'):
+        for sentence in bnc.sents(filename):
+            yield sentence
+    
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     return parser.parse_args()
@@ -32,14 +54,20 @@ def parse_args():
 def main():
     start  = time()
     args = parse_args()
-    nltk.data.path.append(r'C:\Users\weka5\nlp\data\2554\download')
-    root = r'C:\Users\weka5\nlp\data\2554\download\Texts'
-    bnc = BNCCorpusReader(root=root,fileids= r'[A-K]/\w*/\w*\.xml')
-    print (bnc.fileids())
-    all_words = bnc.words()
-    print (all_words)
-    sentences = bnc.sents(fileids='A/A0/A00.xml')
-    print("Sample sentence:", sentences[0])    
+ 
+    bnc = BNC()
+    for file in bnc.filenames():
+        print (file)
+  
+    for word in bnc.words():
+        print (word)
+ 
+    for sentence in bnc.sentences():
+        print (sentence)
+    #sentences = bnc.sents(fileids='A/A0/A00.xml')
+    #print (len(sentences))
+    #for i in range(10):
+        #print(sentences[i])    
     
     elapsed = time() - start
     minutes = int(elapsed/60)
