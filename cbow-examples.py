b402f0e899965f098c21c6aaa2ad2c261539bba1
Simon Crase <simon@greenweaves.nz>
2026-08-10 15:50:44 +1200

    #90 Doco

--- cbow-examples.py
+++ cbow-examples.py
@@ -16,14 +16,16 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
-    Read sentences from a corpus
+    Read sentences from a corpus and generate words 
+    and contexts to we can train CBOW
 '''
 
 from argparse import ArgumentParser
-import csv
+from csv import writer, QUOTE_MINIMAL
 from os.path import join
 from pathlib import Path
 from time import time
+from typing import TextIO
 
 import numpy as np
 import nltk
@@ -33,26 +35,24 @@ from vocabulary import Vocabulary
 from shared.utils import Logger, user_has_requested_stop, get_seed
 from tokenizer import generate_sentences, generate_text, generate_tokens, Token
 
+
 class BNC:
     '''
     Read text from BNC corpus
-    '''    
-    def __init__(self,root = r'./data/2554/download\Texts'):
+    '''
+
+    def __init__(self, root=r'./data/2554/download\Texts'):
         nltk.data.path.append(r'./data\2554\download')
-        self.bnc = BNCCorpusReader(root=root,fileids= r'[A-K]/\w*/\w*\.xml')        
-    
+        self.bnc = BNCCorpusReader(root=root, fileids=r'[A-K]/\w*/\w*\.xml')
+
     def filenames(self):
         '''
         This generator is used to iterate through filenames
         '''
         for filename in self.bnc.fileids():
             yield filename
-            
-    def words(self):
-        for word in self.bnc.words():
-            yield word
-            
-    def sentences(self,path):
+
+    def sentences(self, path):
         '''
         This generator is used to iterate through sentences in a specified file
         
@@ -62,23 +62,37 @@ class BNC:
         for sentence in self.bnc.sents(fileids=path):
             yield sentence
 
+
 class ExampleSet:
-    def __init__(self,window_size=2):
-        self.vocabulary = Vocabulary()
+    '''
+    This class holds the words and contexts for one senetnce
+    '''
+
+    def __init__(self, window_size : int =4, vocabulary:Vocabulary=Vocabulary()):
+        self.vocabulary = vocabulary
         self.SOS = self.vocabulary.tokenize('<SOS>')
         self.EOS = self.vocabulary.tokenize('<EOS>')
         self.window_size = window_size
-        
-    def build(self,sentence,out_file):      
-        self.__accumulate__((
-                            [self.SOS] +
-                            [self.vocabulary.tokenize(word) for word in sentence] +
-                            [self.EOS]   
-                            ),out_file)
 
-
+    def build(self, sentence : [str], out_file : TextIO):
+        '''
+        Construct words and contexts for one sentence
         
-    def __accumulate__(self, tokens: [int],out_file):
+        Parameters:
+            sentence
+            out_file
+        '''
+        self.__accumulate__(self.__tokenize__(sentence), out_file)
+
+    def __tokenize__(self,sentence : [str]) -> [int]:
+        '''
+        Convert senetce fro a list of words to a list of tokens
+        '''
+        return ([self.SOS] +
+                [self.vocabulary.tokenize(word) for word in sentence] +
+                [self.EOS])
+    
+    def __accumulate__(self, tokens: [int], out_file:TextIO):
         '''
         Convert a sequence of tokens, representing one sentence, to 
         words and contexts
@@ -86,8 +100,7 @@ class ExampleSet:
         Parameters:
             tokens
         '''
-        writer = csv.writer(out_file, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)          
+        out = writer(out_file, delimiter=',', quotechar='|', quoting=QUOTE_MINIMAL)
         start = 0
         end = start + 2 * self.window_size + 1
         n_entries = len(tokens) - end + 1
@@ -95,43 +108,46 @@ class ExampleSet:
             run = [tokens[i] for i in range(start, end)]
             words = run[self.window_size]
             context = run[:self.window_size] + run[self.window_size + 1:]
-            writer.writerow([words]+context)
+            out.writerow([words] + context)
             start += 1
             end += 1
-    
+
 
 def parse_args():
     data = './data'
     logs = './logs'
-    window_size = 4    
+    window_size = 4
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--data', default=data, help=f'Path to data files [{data}]')
     parser.add_argument('--logs', default=logs, help=f'Location for storing log files [{logs}]')
     parser.add_argument('-o', '--output', default=None, required=True, help='File name for storing results')
-    parser.add_argument('-m', '--window_size', type=int, default=window_size, 
-                                help=f'Half size of window (context extends left and right) [{window_size}]')    
+    parser.add_argument('-m', '--window_size', type=int, default=window_size,
+                        help=f'Half size of window (context extends left and right) [{window_size}]')
     return parser.parse_args()
-    
+
+
 def main():
-    start  = time()
+    start = time()
     args = parse_args()
     with Logger(Path(__file__).stem, path=args.logs) as _:
-        out = Path(f'{args.data}/{args.output}')
-        Logger.get_instance().log(f'{__file__} {Logger.get_line()} Writing reults to {out}')
+        out_root_path = Path(f'{args.data}/{args.output}')
+        Logger.get_instance().log(f'{__file__} {Logger.get_line()} Writing results to {out_root_path}')
         bnc = BNC()
+        vocabulary = Vocabulary()
         for path in bnc.filenames():
             Logger.get_instance().log(f'{__file__} {Logger.get_line()} Opened {path}')
-            out_file_path = (out / path).with_suffix('.csv')
+            out_file_path = (out_root_path / path).with_suffix('.csv')
             out_file_path.parent.mkdir(parents=True, exist_ok=True)
-            with open(out_file_path,'w',newline='') as out_file:
-                examples = ExampleSet(window_size=args.window_size)
+            examples = ExampleSet(window_size=args.window_size, vocabulary=vocabulary)
+            with open(out_file_path, 'w', newline='') as out_file:
                 for sentence in bnc.sentences(path=path):
-                    examples.build(sentence,out_file)
-      
+                    examples.build(sentence, out_file)
+
         elapsed = time() - start
-        minutes = int(elapsed/60)
-        seconds = elapsed - 60*minutes
+        minutes = int(elapsed / 60)
+        seconds = elapsed - 60 * minutes
         Logger.get_instance().log(f'{__file__} {Logger.get_line()} Elapsed Time {minutes} m {seconds:.2f} s')
-    
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main()
