63fae580f4311a5427c9f53041f527862b27e307
Simon Crase <simon@greenweaves.nz>
2026-08-12 12:39:54 +1200

    #90 Added unit tests for cross entropy

--- cbow2.py
+++ cbow2.py
@@ -19,6 +19,7 @@
 
 from unittest import main,TestCase
 import numpy as np
+from numpy.testing import assert_array_equal,assert_array_almost_equal
 
 class OneHotFactory:
     '''
@@ -56,17 +57,86 @@ class Model:
         X = self.encoder.encode(feature)
         return self.forward(X)
     
-class LogSoftmax:
+class CrossEntropyLoss:
+    '''
+    Used to calculate cross entropy loss
     
+    See A Brief Overview of Cross Entropy Loss, Chris Hughes,
+    https://medium.com/@chris.p.hughes10/a-brief-overview-of-cross-entropy-loss-523aa56b75d5
+    '''
     def log_softmax(self,z):
         return z - np.log(np.exp(z).sum())
     
-    def _call__(self,prediction,label):
+    def log_safe_softmax(self,z):
+        '''
+        Numerically stable version Goodfellow et al, eq (6.33)
+        '''
+        return self.log_softmax(z - z.max())
+    
+    def _cross_entropy(self,log_probabilities,label):
+        return -np.dot(log_probabilities,label).sum()
+    
+    def __call__(self,prediction,label):
         '''
         Parameters:
             prediction
             label
         '''
+        return self._cross_entropy(self.log_softmax(prediction),label)
+
+class TestCrossEntropy(TestCase):
+    '''
+    Tests for the CrossEntropyLoss class
+    '''
     
+    def setUp(self):
+        self.loss_fn = CrossEntropyLoss()
+        
+    def test_brief1(self):
+        '''
+        An example from Chris Hughes' article. He has rounded values to 3 decimal places
+        '''
+        self.assertAlmostEqual(0.357,
+                            self.loss_fn._cross_entropy(
+                                np.log(
+                                    np.array([0.7,0.2,0.1])),
+                                np.array([1,0,0])),
+                            delta=0.001)
+    def test_brief2(self):
+        '''
+        An second example from Chris Hughes' article.
+        '''
+        self.assertAlmostEqual(0.105,
+                            self.loss_fn._cross_entropy(
+                                np.log(
+                                    np.array([0.9,0.05,0.05])),
+                                np.array([1,0,0])),
+                            delta=0.001) 
+        
+    def test_brief3(self):
+        '''
+        An third example from Chris Hughes' article.
+        '''
+        self.assertAlmostEqual(2.303,
+                            self.loss_fn._cross_entropy(
+                                np.log(
+                                    np.array([0.1,0.8,0.1])),
+                                np.array([1,0,0])),
+                            delta=0.001)      
+        
+    def test_log_safe_softmax(self):
+        '''
+        Verify that log_safe_softmax gies the same answer as log_softmax
+        '''
+        assert_array_almost_equal(
+            self.loss_fn.log_softmax(np.array([1,2,3,4])),
+            self.loss_fn.log_safe_softmax(np.array([1,2,3,4]))
+        )
+        assert_array_almost_equal(
+            self.loss_fn.log_softmax(np.array([0.9,0.05,0.025,0.025])),
+            self.loss_fn.log_safe_softmax(np.array([0.9,0.05,0.025,0.025]))
+        )        
+ 
+        
 if __name__=='__main__':
         main()
