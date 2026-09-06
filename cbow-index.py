4477b4bd358806989f9fc027621599bf4e01c09b
Simon Crase <simon@greenweaves.nz>
2026-09-07 09:54:10 +1200

    Add --verbose flag to cbow-index.py

--- cbow-index.py
+++ cbow-index.py
@@ -16,7 +16,8 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
-Construct an index for all file in example set
+Construct an index for all file in example set, 
+so we can use a ramdomizing data loader
 '''
 
 from argparse import ArgumentParser
@@ -24,7 +25,8 @@ from csv import reader,writer,QUOTE_MINIMAL
 from pathlib import Path
 from time import time
 
-import numpy as np
+__version__ = '1.0'
+__author__ = 'Simon Crase'
 
 def parse_args():
     '''
@@ -35,11 +37,22 @@ def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--data', default=data, help=f'Path to data files [{data}]')
     parser.add_argument('--examples', default=examples, help=f'Path to examples files [{examples}]')
+    parser.add_argument('--verbose',default=False,action='store_true',help='Used to print counts')
     return parser.parse_args()
-    
+
+def get_line_count(path):
+    '''
+    Count lines in a csv file
+    '''
+    n = 0
+    with open(path, newline='') as in_file:
+        for row in reader(in_file, delimiter=','):
+            n  += 1
+    return n
+            
 def main():
     '''
-    Read file and create index
+    Read all files in example set and create index file
     '''
     start  = time()
     args = parse_args()
@@ -50,11 +63,9 @@ def main():
         for path in root_dir.rglob("*.csv"):
             if not path.is_file(): continue
             if out_file_path == path: continue    # We don't want to include index file in our counts
-            n = 0
-            with open(path, newline='') as in_file:
-                for row in reader(in_file, delimiter=','):
-                    n  += 1
-            out.writerow([path,n])  
+            out.writerow([path,get_line_count(path)])
+            if args.verbose:
+                print (path,get_line_count(path))
             
     elapsed = time() - start
     minutes = int(elapsed/60)
